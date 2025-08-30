56fccec88b6f602bcea430eb488b6d6059b32cf1
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-08-30 22:05:10 +0100

    Fix unrelated histories

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function SignUpPage() {
-  return <SignUp routing="path" path="/sign-up" />;
+export default function Page() {
+  return <SignUp />;
 }
